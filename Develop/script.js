593c9543864813c235c13e9a5399cfc0deff1316
Efren Leal <efren@Efrens-MacBook-Pro-2.local>
2023-11-28 12:50:17 -0600

    added javascript functions to make prompts accessible

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,38 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var password_el = document.querySelector("#password");
-var length_el = document.querySelector("#length");
-var uppercase_el = document.querySelector("#uppercase");
-var lowercase_el = document.querySelector("#lowercase");
-var numbers_el = document.querySelector("#numbers");
-var symbols_el = document.querySelector("#symbols");
+
+var password_el = document.querySelector('#Password');
+var length_el = document.querySelector('#Length');
+var uppercase_el = document.querySelector('#Uppercase');
+var lowercase_el = document.querySelector('#Lowercase');
+var numbers_el = document.querySelector('#Numbers');
+var symbols_el = document.querySelector('#Symbols');
+
+var generate_btn = document.querySelector('#generate');
+
+var uppercase_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase_chars = "abcdefghijklmnopqrstuvwxyz";
+var numbers_chars = "0123456789";
+var symbols_chars = "!@#$%^&*()";
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function GeneratePassword () {
+  let password = "";
+  let length = length_el.value;
+  let chars = "";
 
-  passwordText.value = password;
+  chars += uppercase_el.checked ? uppercase_chars : "";
+  chars += lowercase_el.checked ? lowercase_chars : "";
+  chars += numbers_el.checked ? numbers_chars : "";
+  chars += symbols_el.checked ? symbols_chars : "";
+
+  for (let i = 0; i <= length; i++) {
+      let rand = Math.floor(Math.random() * chars.length);
+      password += chars.substring(rand, rand + 1);
+}
+
+password_el.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generate_btn.addEventListener("click", GeneratePassword);
